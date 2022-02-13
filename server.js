202e2dcb96926ff1527dcb98b7b847d0c5cc6aca
Steve Bour <bourpower@gmail.com>
2022-02-13 16:52:03 -0500

    server file written

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+let notes = require('./db/db.json')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = Math.floor((1 + Math.random()) * 0x10000)
+    notes.push(req.body);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    for (let i = 0; i < notes.length; i++) {
+        if (req.params.id == notes[i].id) {
+            console.log(req.params.id, notes[i].id)
+            notes = notes.splice(notes[i], 1)
+        } 
+    }
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+})
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
